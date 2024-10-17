6365e89541df7f6898fc3f02d76429a81ad60d9b
Luan <luan.donato10@gmail.com>
2024-10-17 02:08:02 -0300

    Criando entity Post com User aninhado

--- src/main/java/workshop/springboot/mongodb/estudo/config/Instantiation.java
+++ src/main/java/workshop/springboot/mongodb/estudo/config/Instantiation.java
@@ -3,26 +3,38 @@ package workshop.springboot.mongodb.estudo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import workshop.springboot.mongodb.estudo.domain.Post;
 import workshop.springboot.mongodb.estudo.domain.User;
+import workshop.springboot.mongodb.estudo.repository.PostRepository;
 import workshop.springboot.mongodb.estudo.repository.UserRepository;
 
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     @Autowired
-    UserRepository repository;
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
 
     @Override
     public void run(String... args) throws Exception {
 
-        repository.deleteAll();
+        postRepository.deleteAll();
+        userRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        repository.saveAll(Arrays.asList(maria, alex, bob));
+        Post post1 = new Post(null, Instant.now(), "Partiu viagem", "Vou viajar para São Paulo abraços", maria);
+        Post post2 = new Post(null, Instant.now(), "Bom dia", "Acordei feliz hoje!", maria);
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
